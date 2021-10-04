8c5587547995b7fed5e1f0f2765186ad1652864c
Sunny <sunnydeng90@gmail.com>
2021-10-04 14:00:16 -0400

    T2Q4 finished

--- drug_classification.py
+++ drug_classification.py
@@ -6,7 +6,7 @@ dataset_file = "./Documents/drug200.csv"
 
 
 # T2Q2; Class will be 'Drug' types
-def generate_pdf_of_instance_distribution():
+def generate_pdf_of_instance_distribution(csv_data):
     csv_data = pandas.read_csv(dataset_file)
     class_names = csv_data['Drug'].unique().tolist()
     class_names.sort()
@@ -15,5 +15,22 @@ def generate_pdf_of_instance_distribution():
     plt.savefig("./Output/drug-distribution.pdf")
 
 
+# T2Q4; Need to convert Sex:[M, F], BP:[L, N, H], Cholesterol:[L, N, H], and Drug:[drugA, drugB, drugC, drugX, drugY] to numerical format
+def convert_to_numerical_format():
+    csv_data = pandas.read_csv(dataset_file)
+    # unordered values
+    set_of_sex_values = pandas.Categorical(csv_data['Sex'], ordered=False).unique().tolist()
+    # ordered values; BP shares values with Cholesterol
+    # reason for sorting is to make it consistent with PDF's order
+    set_of_drug_values = pandas.Categorical(csv_data['Drug'], ordered=False).unique().tolist()
+    set_of_drug_values.sort()
+    set_of_bp_values = pandas.Categorical(csv_data['BP'], ordered=True, categories=['LOW', 'NORMAL', 'HIGH'])
+
+    processed_data_frame = csv_data.replace(to_replace=set_of_sex_values, value=range(len(set_of_sex_values)))
+    processed_data_frame = processed_data_frame.replace(to_replace=set_of_drug_values, value=range(len(set_of_drug_values)))
+    processed_data_frame = processed_data_frame.replace(to_replace=set_of_bp_values.categories.tolist(), value=range(len(set_of_bp_values.categories.tolist())))
+    return processed_data_frame
+
+
 def prep_classifier_for_analysis():
-    generate_pdf_of_instance_distribution()
+    processed_dataset = convert_to_numerical_format()
