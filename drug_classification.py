564bdb683c6c0ca9320d582715e886d407f6fa7f
WilliamM-L <w.morin.laberge@gmail.com>
2021-10-07 21:26:34 -0400

    Refactored drug_classification + did until 6.e

--- drug_classification.py
+++ drug_classification.py
@@ -17,65 +17,72 @@ def generate_pdf_of_instance_distribution(csv_data):
     plt.savefig("./Output/drug-distribution.pdf")
 
 
-# T2Q4; Need to convert Sex:[M, F], BP:[L, N, H], Cholesterol:[L, N, H], and Drug:[drugA, drugB, drugC, drugX, drugY] to numerical format
-def convert_to_numerical_format():
-    csv_data = pandas.read_csv(dataset_file)
-    # unordered values
-    set_of_sex_values = pandas.Categorical(csv_data['Sex'], ordered=False)
-    sex_columns = pandas.get_dummies(set_of_sex_values, prefix='Sex')
-    csv_data = csv_data.drop(labels='Sex', axis=1)
-    csv_data = csv_data.join(sex_columns)
-    # ordered values; BP shares values with Cholesterol
-    # reason for sorting is to make it consistent with PDF's order
-    set_of_bp_values = pandas.Categorical(csv_data['BP'], ordered=True, categories=['LOW', 'NORMAL', 'HIGH'])
-    ordered_bp_list = set_of_bp_values.categories.tolist()
+def preprocess_data():
+    # T2Q4; Need to convert Sex:[M, F], BP:[L, N, H] and Cholesterol:[L, N, H] to numerical format
+    def convert_to_numerical_format():  # inner function since it's only used by prep_classifier_for_analysis
+        csv_data = pandas.read_csv(dataset_file)
+        # unordered values
+        set_of_sex_values = pandas.Categorical(csv_data['Sex'], ordered=False)
+        sex_columns = pandas.get_dummies(set_of_sex_values, prefix='Sex')
+        csv_data = csv_data.drop(labels='Sex', axis=1)
+        csv_data = csv_data.join(sex_columns)
+        # ordered values; BP shares values with Cholesterol
+        # reason for sorting is to make it consistent with PDF's order
+        set_of_bp_values = pandas.Categorical(csv_data['BP'], ordered=True, categories=['LOW', 'NORMAL', 'HIGH'])
+        ordered_bp_list = set_of_bp_values.categories.tolist()
 
-    processed_data_frame = csv_data.replace(to_replace=ordered_bp_list, value=range(len(ordered_bp_list)))
-    # dropped to put into separate list
-    processed_data_frame = processed_data_frame.drop(labels='Drug', axis=1)
-    return processed_data_frame, csv_data['Drug'].tolist()
+        processed_data_frame = csv_data.replace(to_replace=ordered_bp_list, value=range(len(ordered_bp_list)))
+        # dropped to put into separate list
+        processed_data_frame = processed_data_frame.drop(labels='Drug', axis=1)
+        return processed_data_frame, csv_data['Drug'].tolist()
 
-
-def prep_classifier_for_analysis():
     processed_dataset, classes_index_dataset = convert_to_numerical_format()
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(processed_dataset, classes_index_dataset)
     return x_train, x_test, y_train, y_test
 
 
 def run_classifiers():
-    x_train, x_test, y_train, y_test = prep_classifier_for_analysis()
+    def get_performance_metrics(classifier, x_test, y_test):
+        metrics = {}
+        predicted_y = classifier.predict(x_test)
+        metrics['confusion_matrix'] = sklearn.metrics.confusion_matrix(y_test, predicted_y)
+        metrics['classification_report'] = sklearn.metrics.classification_report(y_test, predicted_y, output_dict=True)
+        metrics['accuracy_score'] = sklearn.metrics.accuracy_score(y_test, predicted_y)
+        metrics['f1_score_macro'] = sklearn.metrics.f1_score(y_test, predicted_y, average='macro')
+        metrics['f1_score_weighted'] = sklearn.metrics.f1_score(y_test, predicted_y, average='weighted')
+        return metrics
+
+    x_train, x_test, y_train, y_test = preprocess_data()
     
-    # Gaussian Naive Bayes
+    # 6.a Gaussian Naive Bayes
     nb_classifier = sklearn.naive_bayes.MultinomialNB()
     nb_classifier.fit(x_train, y_train)
-    nb_predicted_y = nb_classifier.predict(x_test)
-    nb_confusion_matrix = sklearn.metrics.confusion_matrix(y_test, nb_predicted_y)
-    nb_classification_report = sklearn.metrics.classification_report(y_test, nb_predicted_y, output_dict=True)
-    nb_accuracy_score = sklearn.metrics.accuracy_score(y_test, nb_predicted_y)
-    nb_f1_score_macro = sklearn.metrics.f1_score(y_test, nb_predicted_y, average='macro')
-    nb_f1_score_weighted = sklearn.metrics.f1_score(y_test, nb_predicted_y, average='weighted')
+    nb_metrics = get_performance_metrics(nb_classifier, x_test, y_test)
 
-    # Base-DT
+
+    # 6.b Base-DT
     b_dt_classifier = sklearn.tree.DecisionTreeClassifier()
     b_dt_classifier.fit(x_train, y_train)
-    b_dt_predicted_y = b_dt_classifier.predict(x_test)
-    b_dt_confusion_matrix = sklearn.metrics.accuracy_score(y_test, b_dt_predicted_y)
-    b_dt_classification_report = sklearn.metrics.classification_report(y_test, b_dt_predicted_y, output_dict=True)
-    b_dt_accuracy_score = sklearn.metrics.accuracy_score(y_test, b_dt_predicted_y)
-    b_dt_f1_score_macro = sklearn.metrics.f1_score(y_test, b_dt_predicted_y, average='macro')
-    b_dt_f1_score_weighted = sklearn.metrics.f1_score(y_test, b_dt_predicted_y, average='weighted')
+    b_dt_metrics = get_performance_metrics(b_dt_classifier, x_test, y_test)
 
-    # Top-DT
+    # 6.c Top-DT
     # values required to play around with:
-    tree_parameters = {'criterion': ['gini','entropy'],'max_depth': [5,10], 'min_samples_split' : [2, 4]}
+    # tree_parameters = {'criterion': ['gini' OR 'entropy'], 'max_depth': [2 values], 'min_samples_split': [3 values]}
+    tree_parameters = {'criterion': ['entropy'], 'max_depth': [5, 10], 'min_samples_split': [2, 4, 6]}
     t_raw_dt_classifier = sklearn.tree.DecisionTreeClassifier()
     t_dt_classifier = sklearn.model_selection.GridSearchCV(t_raw_dt_classifier, tree_parameters)
     t_dt_classifier.fit(x_train, y_train)
-    t_dt_predicted_y = t_dt_classifier.predict(x_test)
-    t_dt_confusion_matrix = sklearn.metrics.confusion_matrix(y_test, t_dt_predicted_y)
-    t_dt_classification_report = sklearn.metrics.classification_report(y_test, t_dt_predicted_y, output_dict=True)
-    t_dt_accuracy_score = sklearn.metrics.accuracy_score(y_test, t_dt_predicted_y)
-    t_dt_f1_score_macro = sklearn.metrics.f1_score(y_test, t_dt_predicted_y, average='macro')
-    t_dt_f1_score_weighted = sklearn.metrics.f1_score(y_test, t_dt_predicted_y, average='weighted')
-    
-run_classifiers()
+    t_dt_metrics = get_performance_metrics(t_dt_classifier, x_test, y_test)
+
+    # 6.d Perceptron
+    perceptron_classifier = sklearn.linear_model.Perceptron()
+    perceptron_classifier.fit(x_train, y_train)
+    perceptron_metrics = get_performance_metrics(perceptron_classifier, x_test, y_test)
+
+    # 6.e Base Multi-Layered Perceptron
+    # hidden_layer_sizes=[100] -> a single hidden layer with 100 neurons
+    # logistic -> sigmoid function
+    # sgd -> stochastic gradient descent
+    MLP_classifier = sklearn.neural_network.MLPClassifier(hidden_layer_sizes=[100], activation='logistic', solver="sgd")
+    MLP_classifier.fit(x_train, y_train)
+    MLP_metrics = get_performance_metrics(MLP_classifier, x_test, y_test)
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import drug_classification as dc
 
 def main():
     # tc.prep_classifier_for_analysis()
-    dc.prep_classifier_for_analysis()
+    dc.run_classifiers()
 
 
 # Always at the bottom of the file and will call main()
